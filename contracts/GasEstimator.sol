793ebeb24ed27ba2bb5313e54e3a34e165ccfa58
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-02 16:06:20 +0400

    Update GasEstimator.sol

--- contracts/GasEstimator.sol
+++ contracts/GasEstimator.sol
@@ -2,12 +2,25 @@
 
 pragma solidity 0.8.19;
 
+/**
+ * @title GasEstimator
+ * @notice The GasEstimator contract provides functions to estimate the gas limit and gas cost of a contract call.
+ */
 contract GasEstimator {
+    /**
+     * @dev Returns the remaining gas limit for the current transaction.
+     * @return gasLimit The remaining gas limit.
+     */
     function gasLimit() external view returns (uint256) {
         return gasleft();
     }
 
-    function gasCost(address target, bytes calldata data) external view returns(uint256 gasUsed, bool success) {
+    /**
+     * @dev Estimates the gas cost and success status of a contract call.
+     * @notice This function executes a staticcall to estimate the gas cost of the contract call.
+     * It returns the difference in gas before and after the call as the gas used and the success status of the call.
+     */
+    function gasCost(address target, bytes calldata data) external view returns (uint256 gasUsed, bool success) {
         uint256 gas = gasleft();
         (bool s, ) = target.staticcall(data);
         return (gas - gasleft(), s);
